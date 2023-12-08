15f84669c1e6bab1260098ee79da33976db5ec4b
mgclmirai <148247449+mgclmirai@users.noreply.github.com>
2023-12-08 18:30:24 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,188 +1,137 @@
-// // типы: string строковые, numbers, boolean
-// let firstname = 'Anel'
-// firstname = 'who?'
-// // // константа не меняется как let
-// const age = 18
-
-// let isStudent = true
-
-// let isCriminal = false
-
-// console.log(firstname, age, isStudent)
-
-// console.log(typeof firstname)
-
-// camelCase
-
-// let myName = 'Anel'
-
-// let isRealStudent
-
-// это все разные операторы
-// let my_name
-// let _name
-// let name_
-// let $name
-
-// так писать нельзя
-// let my-name
-// let 1name
 
 
-// let a = 17
-// let b = 10
+// for (let i = 1; i <= num; i++){
+//     let a = num/2;
+//     if (i===a){
+//         continue;
+//     }
 
-// console.log(a + b)
-// console.log(a - b)
-// console.log(a * b)
-// console.log(a / b)
-
-// console.log(17 % 10)
-
-// console.log(10 % 10)
-
-// let text = prompt()
-
-// console.log(text)
-
-// let firstname = "Anel"
-// let lastname = "ME"
-
-// console.log(firstname+' '+lastname)
-
-
-
-// let firstname = prompt("Напишите имя")
-// let lastname = prompt("и фамилию")
-
-// console.log ('Hello'+ ' ' +firstname + ' '+lastname +'!')
-
-
-
-// let a = '10'
-// let b = 10
-
-// console.log(typeof a, " - type '10' ")
-// console.log(typeof b, " - type 10 ")
-
-
-
-// number()
-
-// let a = '10'
-
-// console.log(typeof a)
-
-// let num1 = +prompt("first num:")
-// let num2 = +prompt("sec num:")
-
-
-//  первый способ
-// console.log(Number(num1) + Number(num2))
-
-// второй способ
-// console.log(num1 + num2)
-// console.log(num1 / num2)
-// console.log(num1 - num2)
-// console.log(num1 * num2)
-
-
-// function greeting(){
-//     console.log('how r u')
+//     console.log(i)
 // }
 
-// greeting()
-// greeting()
-// greeting()
+// let paragraph = document.querySelector("div p")
+// console.log(paragraph)
 
-// function quad(a){
-//     return a * a
+// paragraph.style.backgroundColor = "yellow";
+// paragraph.style.fontSize = "200%";
+// paragraph.style.padding = "18px";
+// paragraph.style.boxShadow = " 3px 3px 6 px black";
+
+// console.log(paragraph)
+
+// let button = document.querySelector("#button")
+
+// function showResult(){
+//     let newElement = document.createElement("p")
+//     newElement.textContent = "you clicked me"
+
+//     document.body.append(newElement)
+
+//     console.log("click", showResult)
 // }
 
-// let res = quad(19)
+// let button = document.querySelector("#button")
+// let input = document.querySelector("#input")
 
-// console.log(res)
+
+
+// function getSquare(){
+//     let num = 15
+//     let i = 2
+
+//     while (i*i < num){
+//         let result = document.createElement("p")
+//         result.textContent = '${i*i}'
+//         document.body.append(result)
+        
+//         ++i
+//      }
+// }
+
+// button.addEventListener('click', getSquare)
 
 
 // 1 exercise
-let inputEl1 = document.getElementById("my-first-input")
-let inputEl2 = document.getElementById("my-second-input")
-let button = document.getElementById("button")
-let resultEl = document.getElementById("result")
-console.log(resultEl.textContent)
-button.onclick=function () {
-    let num = Number(inputEl1.value) + Number(inputEl2.value)
-    resultEl.textContent = num
+function Factorial() {
+    let inputNumber = document.getElementById("numberInput").value;
+
+    let factorial = 1;
+    for (let i = 2; i <= inputNumber; i++) {
+        factorial *= i;
+    }
+    let resultElement = document.createElement("p");
+    resultElement.textContent = factorial;
+    document.body.append(resultElement)
 }
+
 
 // 2 exercise
-let inputNum = document.getElementById("inputNum")
-let buttonNum = document.getElementById("buttonNum")
-let resultNum = document.getElementById("result2")
-buttonNum.onclick = function(){
-  if (inputNum.value > 0) {
-    resultNum.textContent = "Positive"
-  } else if (inputNum.value == 0) {
-    resultNum.textContent = "They are even"
-  } else {
-    resultNum.textContent = "Negative"
-  }
-}
+function degree() {
+    let degree = document.getElementById("degree").value;
 
-
-
-
-// 3 задание
-let inputEvenCheck = document.getElementById("inputEvenCheck")
-let buttonEvenCheck = document.getElementById("buttonEvenCheck")
-let resultEvenCheck = document.getElementById("result3")
-buttonEvenCheck.onclick = function(){
-  if (+inputEvenCheck.value % 2 == 0) {
-    resultEvenCheck.textContent ="Even"
-  } else if (+resultEvenCheck % 2 !=1) {
-    resultEvenCheck.textContent ="Odd"
-  }
-}
-
-
-// 4 задание
-
-document.getElementById("calculate").addEventListener("click", function () {
-    let input1Value = Number(document.getElementById("input1").value);
-    let input2Value = Number(document.getElementById("input2").value);
-  
-    let plusResult = input1Value + input2Value;
-    let minusResult = input1Value - input2Value;
-    let multiplicationResult = input1Value * input2Value;
-  
-    let divisionResult =
-      input2Value !== 0 ? input1Value / input2Value : "error";
-  
-    document.getElementById("plusResult").textContent =
-      "Addition: " + plusResult;
-    document.getElementById("minusResult").textContent =
-      "Subtraction: " + minusResult;
-    document.getElementById("multiplicationResult").textContent =
-      "Multiplication: " + multiplicationResult;
-    document.getElementById("divisionResult").textContent =
-      "Division: " + divisionResult;
-})
-
-// 5 задание
-
-document.getElementById("getFat").addEventListener("click", function () {
-    let height = Number(document.getElementById("height").value);
-    let waist = Number(document.getElementById("waist").value);
-    let gender = document.getElementById("gender").value;
-    let fatPercentage;
-    if (waist !== 0) {
-      if (gender==="male") {
-        fatPercentage =64-(20*height)/waist;
-      }
-    } 
-    else {
-      fatPercentage = "error";
+    let result1 = 1;
+    for (let i = 0; i < degree; i++) {
+        result1 *= 2;
     }
-    document.getElementById("fatPercentage").textContent =
-      "Fat Percentage:" + fatPercentage.toFixed(2) + "%";
-  })
+
+    let resultElement = document.getElementById("result1");
+    resultElement.textContent = result1;
+}
+
+
+// 3 exercise
+function find() {
+    let input = document.getElementById("input").value;
+
+    let small = 2;
+    while (small <= input) {
+        if (input % small === 0) {
+            break;
+        }
+        small++;
+    }
+
+    let resultElement = document.getElementById("result2");
+    resultElement.textContent = small;
+}
+
+
+// 4 exercise
+function findDivide() {
+    let inputNumber = document.getElementById("number").value;
+
+    let divide = [];
+    for (let i = 1; i <= Math.sqrt(inputNumber); i++) {
+        if (inputNumber % i === 0) {
+            divide.push(i);
+            if (i !== inputNumber / i) {
+                divide.push(inputNumber / i);
+            }
+        }
+    }
+    divide.sort((a, b) => a - b);
+    
+    let resultElement = document.getElementById("result3");
+    resultElement.textContent = `Делители числа ${inputNumber}: ${divide.join(', ')}`;
+}
+
+
+// 7 exercise
+function check() {
+    let inputNumber = document.getElementById("Prime").value;
+
+    let isPrime = true;
+    for (let i = 2; i <= Math.sqrt(inputNumber); i++) {
+        if (inputNumber % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    let resultElement = document.getElementById("result7");
+    if (isPrime) {
+        resultElement.textContent = `${inputNumber} - prime number`;
+    } else {
+        resultElement.textContent = `${inputNumber} - not prime number`;
+    }
+}
